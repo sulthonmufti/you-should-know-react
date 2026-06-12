48c261334850f37756705d3c6b47bb09789c21fe
sulthonmufti <msulthonmufti14@gmail.com>
2026-06-12 14:34:27 +0700

    feat: setup global style and design tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Import desain token & global styling
+import './styles/variables.css'
+import './styles/global.css'
+import './styles/typography.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
